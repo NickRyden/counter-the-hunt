76c64c7b03d97a00faf682745a7fa359aaac3a77
NickRyden <nick.ryden@hotmail.com>
2022-10-22 09:58:36 +1100

    Update bluetooth_scanner.py

--- bluetooth_scanner.py
+++ bluetooth_scanner.py
@@ -27,22 +27,19 @@ def scan_services():
     number_of_devices = len(devices)
     print(number_of_devices, "devices found")
     for addr,name in devices:
+        print("\n")
+        print("Device Name: %s" % (name))
+        print("Device MAC Address: %s" % (addr))
+        print("Services Found:")
 
-    print("\n")
-    print("Device Name: %s" % (name))
-    print("Device MAC Address: %s" % (addr))
-    print("Services Found:")
-    
-    services = bluetooth.find_service(address=addr)
+        services = bluetooth.find_service(address=addr)
 
-    if len(services) <=0:
-
-    print("zero services found on", addr)
-
-    else:
-        for serv in services:
-            print(serv['name'])
-            print("\n")
+        if len(services) <=0:
+            print("zero services found on", addr)
+        else:
+            for serv in services:
+                print(serv['name'])
+                print("\n")
     
     return()
 
