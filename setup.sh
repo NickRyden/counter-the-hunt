0db30d76c4e157b2da4b90268af8c05bdb641327
NickRyden <nick.ryden@hotmail.com>
2022-10-22 08:03:35 +1100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install -y python 3.10
+
+pip3 install scapy
+pip3 install kismet
+pip3 install kismetdb
+pip3 install kismet_rest
+pip3 install pandas
