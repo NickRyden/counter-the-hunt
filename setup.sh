8fa92940d3b49744c5bee6a257247c300a86f0ea
NickRyden <nick.ryden@hotmail.com>
2022-10-22 08:47:31 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install -y python 3.10
 sudo apt-get install -y aircrack-ng
+sudo apt install bluetooth libbluetooth-dev
 
 pip3 install scapy
 pip3 install kismet
 pip3 install kismetdb
 pip3 install kismet_rest
 pip3 install pandas
+pip3 install pybluez
