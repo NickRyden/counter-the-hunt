8d9bc6fe140af4b695fec003bb6fc45a7c633a65
NickRyden <nick.ryden@hotmail.com>
2022-10-24 13:43:04 +1100

    Update vendor.py

--- vendor.py
+++ vendor.py
@@ -1,7 +1,11 @@
-def csvCompare(mac):
+from csv import DictReader
+
+def getVendor(mac):
 	with open(filename) as filp:
 		reader = DictReader(filp)
 
 		for row in reader:
 			if (row['vendor_mac'] == mac):
 				return dict(row)
+
+print(getVendor('0000'))
