e8d29675c7483345e5fe2bb17f38458b5f33209c
NickRyden <nick.ryden@hotmail.com>
2022-10-24 14:23:08 +1100

    Update kismet_seconds.py

--- kismet_seconds.py
+++ kismet_seconds.py
@@ -6,4 +6,13 @@ def getEpochTime():
     delta = abs(dt - kismet_time)
     return delta.total_seconds()
 
+def createAlertInterval(alert_length):
+    # Return an alert in seconds minus the alert length
+    return getEpochTime() - (alert_length * 60)
+
 print(getEpochTime())
+print(createAlertInterval(5))
+print(createAlertInterval(10))
+print(createAlertInterval(15))
+print(createAlertInterval(20))
+print(createAlertInterval(25))
