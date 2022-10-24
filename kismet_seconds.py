c811ea131b1b1d6dcb347bed4cd4b675ecc4a6d8
NickRyden <nick.ryden@hotmail.com>
2022-10-24 14:17:50 +1100

    Update kismet_seconds.py

--- kismet_seconds.py
+++ kismet_seconds.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 
-def getSeconds():
+def getEpochTime():
     dt = datetime(2018, 10, 22, 0, 0)
-    kismet_time = datetime(2019, 1, 1)
+    kismet_time = datetime(1970, 1, 1)
     delta = abs(dt - kismet_time)
     return delta.total_seconds()
 
-print('Datetime to Seconds since kismet:', getSeconds())
+print(getEpochTime())
