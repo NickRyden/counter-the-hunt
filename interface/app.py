2adb3e9c11191ed5e3a2df0da0f29d9d44f081ef
NickRyden <nick.ryden@hotmail.com>
2022-10-26 11:30:29 +1100

    Update app.py

--- interface/app.py
+++ interface/app.py
@@ -1,4 +1,10 @@
+import sqlite3
 from flask import Flask, render_template
+
+def conn():
+    dbconn = sqlite3.connect('database.db')
+    dbconn.row_factory = sqlite3.Row
+    return conn
 
 app = Flask(__name__)
 
