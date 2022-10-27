e2b297e1289143e91763d9fcad5ad617124f7c67
NickRyden <nick.ryden@hotmail.com>
2022-10-27 16:58:19 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,44 +16,10 @@ sudo apt install kismet
 # Python pips
 pip3 install scapy
 pip3 install kismet
-pip3 install kismetdb
-pip3 install kismet_rest
-pip3 install pandas
 pip3 install pybluez
-pip3 install kismet-analyzer
-
+pip3 install PyQt5
 
 # Source: https://www.freecodecamp.org/news/wireless-security-using-raspberry-pi-4-kismet-and-python/
 
 # Set a SUID for kismet for security
 sudo usermod --append --groups kismet $USER
-
-# Create a SSL encryption to use Kismet
-openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 365
-
-# Kismet web override
-#!/bin/bash
-# Kismet setup
-usb_driver=rt2800usb
-wlan=$(ls /sys/bus/usb/drivers/$usb_driver/*/net/)
-if [ $? -eq 0 ]; then
-    set -ex
-    /usr/sbin/ifconfig "$wlan" down
-    /usr/sbin/iwconfig "$wlan" mode monitor
-    /usr/sbin/ifconfig "$wlan" up
-    set +ex
-    /bin/cat<<KISMETOVERR>/etc/kismet/kismet_site.conf
-server_name=Nunez Barrios Kismet server
-logprefix=/data/kismet
-source=$wlan
-httpd_ssl=true
-httpd_ssl_cert=/etc/pki/raspberrypi/raspberry-server.csr
-httpd_ssl_key=/etc/pki/raspberrypi/raspberry-server-key.pem
-KISMETOVERR
-fi
-
-# start Kismet - non-root
-kismet
-# access from http://localhost:2501
-
-print_f("Access from http://localhost:2501, First time must setup Username and Password");
