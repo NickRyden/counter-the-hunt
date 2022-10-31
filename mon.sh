e4c296f2de359efc06d063bd241207369ea38662
NickRyden <nick.ryden@hotmail.com>
2022-11-01 08:35:15 +1100

    Create mon.sh

---
+++ mon.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Set adapter to monitor mode
+interface=$(iw dev | awk '$1=="Interface"{print $2}')
+sudo airmon-ng check kill
+sudo airmon-ng start $interface
+
+# Adapter has changed, use this insetad.
+interface=$(iw dev | awk '$1=="Interface"{print $2}')
+kismet -c $interface
