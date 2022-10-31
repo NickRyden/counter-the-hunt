443b388f3f7be6b2dc2de1475085355f2e65ecc0
NickRyden <nick.ryden@hotmail.com>
2022-11-01 08:45:06 +1100

    Update mon.sh

--- mon.sh
+++ mon.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# !IMPORTANT - Run at first start
+# sudo usermod --append --groups kismet $USER
+# AND
+# Create a username and password in the Kismet web interface at http://localhost:2501
+
 # Set adapter to monitor mode
 interface=$("iw dev | awk '$1==\"Interface\"{print $2}'")
 sudo airmon-ng check kill
