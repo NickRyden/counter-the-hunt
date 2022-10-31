5395be0a602b450a92fe531c95481d1612919648
NickRyden <nick.ryden@hotmail.com>
2022-11-01 08:41:28 +1100

    Update mon.sh

--- mon.sh
+++ mon.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Set adapter to monitor mode
-interface=$(iw dev | awk '$1=="Interface"{print $2}')
+interface=$("iw dev | awk '$1==\"Interface\"{print $2}'")
 sudo airmon-ng check kill
 sudo airmon-ng start $interface
 
 # Adapter has changed, use this insetad.
-interface=$(iw dev | awk '$1=="Interface"{print $2}')
+interface=$("iw dev | awk '$1==\"Interface\"{print $2}'")
 kismet -c $interface
+sudo aireplay-ng $interface
