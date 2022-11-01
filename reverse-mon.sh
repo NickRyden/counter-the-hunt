99280f6bbf195cd629ac73b4aac1647969b012c5
NickRyden <nick.ryden@hotmail.com>
2022-11-01 17:11:53 +1100

    Update reverse-mon.sh

--- reverse-mon.sh
+++ reverse-mon.sh
@@ -1,13 +1,14 @@
-#!/bin/bash
+#/bin/bash
 
-# Kill Kismet
-pid=$(pidof kismet)
-kill pid
+# Find the process ID and stop Kismet
+process_id=$(pidof kismet)
+kill $process_id
 
-# Reset connection
-interface=$("iw dev | awk '$1=="Interface"{print $2}'")
-airmon-ng stop $interface
-ifconfig $interface up
+# Stop monitor mode and restore internet connectivity on the wireless adapter
+interface=$(iw dev | awk '$1=="Interface"{print $2}') 
+sudo airmon-ng stop $interface
 
-# Restart the Network manager
+interface=$(iw dev | awk '$1=="Interface"{print $2}') 
+sudo ifconfig $interface up
+
 sudo service NetworkManager restart
